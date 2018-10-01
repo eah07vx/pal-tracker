71842ca23c141587613e62164b91b3da8c7f1f46
nikhil and chris <nikhil.patel@mckesson.com>
2018-10-01 14:56:14 -0600

    scaling assgn

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,35 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private final String port, memLimit, cfIndex, cfInstance;
+
+    public EnvController(@Value("${PORT:NOT SET}") String port,
+                              @Value("${MEMORY_LIMIT:NOT SET}") String memLimit,
+                              @Value("${CF_INSTANCE_INDEX:NOT SET}") String cfIndex,
+                              @Value("${CF_INSTANCE_ADDR:NOT SET}") String cfInstance) {
+        this.port = port;
+        this.memLimit = memLimit;
+        this.cfIndex = cfIndex;
+        this.cfInstance = cfInstance;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        Map<String, String> env = new HashMap<>();
+
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memLimit);
+        env.put("CF_INSTANCE_INDEX", cfIndex);
+        env.put("CF_INSTANCE_ADDR", cfInstance);
+        return env;
+    }
+}
