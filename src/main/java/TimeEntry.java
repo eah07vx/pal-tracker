ce7494672889378491719a36b2c8841fbfbf389b
nikhil and chris <nikhil.patel@mckesson.com>
2018-10-02 09:31:01 -0600

    Added Time Entry

---
+++ src/main/java/TimeEntry.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class TimeEntry {
+    private long id;
+    private long projectId;
+    private long userId;
+    private LocalDate date;
+    private int hours;
+}
+
